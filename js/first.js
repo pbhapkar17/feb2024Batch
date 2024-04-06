f921d5f81f1fab282b23e2e6743b7151a1d3fae6
pbhapkar17 <poonambhapkar34@gmail.com>
2024-04-06 22:02:12 +0530

    intro to variables

---
+++ js/first.js
@@ -0,0 +1,28 @@
+// Varibles: are the container to hold the Data.
+//let/var/const/no an keyword
+// syntax reserved_keywords variableName = value;
+
+//variname >>space not allowed
+//special characters except _$ not allowed
+//vn always starts with alphabet,intergers not allowed
+
+let cityName;  //dclaration
+let _cityName;
+let CityName;
+let cityname;
+let city_name;
+
+let name1; //declration
+console.log("name1 before init",name1);
+name1 = "Poonam"; //defination
+console.log("name1 after init",name1 );
+
+let name2 = "Ram";  //decl+def
+// name2 = "Raj";
+// name2 = "Om";
+
+var city = "Pune";
+console.log("city ",city);
+const fruit = "Mango";
+
+color = "Red";  //default var keyword
